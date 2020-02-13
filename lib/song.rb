92797b09b466f862bf3110d9f7cc852367273409
Mark Drehmann <markdrehmann@gmail.com>
2020-02-13 02:58:29 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,15 @@
 class Song
 
   @@count = 0
+  @@artists = []
+  @@genres = []
   attr_accessor :name, :artist, :genre
 
   def initialize(name, artist, genre)
     @@count += 1
     @name = name
-    @artist = artist
-    @genre = genre
+    @@artists << @artist = artist
+    @@genres << @genre = genre
   end
 
   def self.count
