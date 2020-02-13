df51d8238af8c1ddce4f41f5b788d150fb106a7b
Mark Drehmann <markdrehmann@gmail.com>
2020-02-13 03:30:02 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -36,4 +36,16 @@ class Song
     hash
   end
 
+  def self.artist_count
+    hash = {}
+    @@artists.each do |artist|
+      if hash[artist]
+        hash[artist] += 1
+      else
+        hash[artist] = 1
+      end
+    end
+    hash
+  end
+
 end
